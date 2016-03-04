5243370d385890829a7f4e1a74e587a29fa6d04f
Maxwell Chow <maxchowla@gmail.com>
2016-03-03 18:07:38 -0800

    Code headers

--- Chapter 1 - Basic Logging-in Bot/login_complex.js
+++ Chapter 1 - Basic Logging-in Bot/login_complex.js
@@ -1,9 +1,8 @@
 // Maxwell Chow, 03 Mar 2016
-// login.js
-// An example script to log in to Steam
+// login_complex.js
+// An example script to log in to Steam - complex version.
 
 var SteamCommunity = require('steamcommunity');
-var steam = new SteamCommunity();
 
 var logOnOptions = {
   'accountName': "CharredBot04",
@@ -11,7 +10,7 @@ var logOnOptions = {
 };
 
 //logs in via browser
-steam.login(logOnOptions, function(err, sessionID, cookies, steamguard) {
+new SteamCommunity().login(logOnOptions, (err, sessionID, cookies, steamguard) => {
 	if (err) {
 		console.log("There was an error logging in! Error details: " + err.message);
 		process.exit(1); //terminates program
